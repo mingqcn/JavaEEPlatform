f56e2c5e2137ca21008e9ec6508adf53778a4feb
Mingqiu <mingqiu@xmu.edu.cn>
2020-10-25 22:50:33 +0800

    initialization mode

--- 4. Redis/RestFulDemoRedis/src/main/java/cn/edu/xmu/restfuldemo/config/RedisConfig.java
+++ 4. Redis/RestFulDemoRedis/src/main/java/cn/edu/xmu/restfuldemo/config/RedisConfig.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -29,6 +31,11 @@ public class RedisConfig  {
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+
+        // 解决jackson2无法反序列化LocalDateTime的问题
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        om.registerModule(new JavaTimeModule());
+
         //指定序列化输入的类型，就是将数据库里的数据按照一定类型存储到redis缓存中。
         //整个类、除final外的的属性信息都需要被序列化和反序列化。
         om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
