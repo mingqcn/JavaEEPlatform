0e3c1694e602a50edcabb91572586128d43bfefe
My Baby <mingqiu@xmu.edu.cn>
2021-06-15 08:09:31 +0000

    jmeter test

--- 2.SpringMvc/RestFulDemo/runjava.sh
+++ 2.SpringMvc/RestFulDemo/runjava.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --
 
-java -Xms1g -Xmx1g -Xss512k \
+java -Xms1g -Xmx1g -Xss256k \
      -Dcom.sun.management.jmxremote.port=3214 \
      -Dcom.sun.management.jmxremote.ssl=false \
      -Dcom.sun.management.jmxremote.authenticate=false \
