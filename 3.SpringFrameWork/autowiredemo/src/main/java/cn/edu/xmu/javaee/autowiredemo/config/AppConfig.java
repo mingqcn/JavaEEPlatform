e4ec4b1bf3792e89446b01b1fabb08913442d2b8
Ming Qiu <mingqiu@xmu.edu.cn>
2022-10-01 15:52:02 +0800

    add Java-based autowired

--- 3.SpringFrameWork/autowiredemo/src/main/java/cn/edu/xmu/javaee/autowiredemo/config/AppConfig.java
+++ 3.SpringFrameWork/autowiredemo/src/main/java/cn/edu/xmu/javaee/autowiredemo/config/AppConfig.java
@@ -6,6 +6,8 @@ import cn.edu.xmu.javaee.autowiredemo.autowiredbean.Car;
 import cn.edu.xmu.javaee.autowiredemo.autowiredbean.Haval;
 import cn.edu.xmu.javaee.autowiredemo.autowiredbean.Office;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -13,15 +15,20 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class AppConfig {
 
+
     @Bean
     @Scope("singleton")
     public Car haval(){
         return new Haval();
     }
 
+    @Value("${company.boss.drive}")
+    private String car_drive;
+
     @Bean
     @Autowired
-    public Boss boss(Car car, Office office){
+    public Boss boss(Office office, ApplicationContext context){
+        Car car= (Car) context.getBean(car_drive);
         return new Boss(car, office);
     }
 }
