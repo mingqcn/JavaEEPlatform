41fbc64e3f6dd1f69b7bdf6443ae25a6c393c67f
Ming Qiu <mingqiu@xmu.edu.cn>
2022-10-04 13:22:33 +0800

    add docker rmi

--- 3.SpringFrameWork/AdminUI/ClearImage.sh
+++ 3.SpringFrameWork/AdminUI/ClearImage.sh
@@ -2,12 +2,7 @@
 # Auth: Ming Qiu  Time: 2022-10-3-13:03
 # remove the specific docker image
 
-printf "      _                   _                             \n"
-printf "  ___| | ___  __ _ _ __  (_)_ __ ___   __ _  __ _  ___  \n"
-printf " / __| |/ _ \/ _` | '__| | | '_ ` _ \ / _` |/ _` |/ _ \ \n"
-printf "| (__| |  __/ (_| | |    | | | | | | | (_| | (_| |  __/ \n"
-printf " \___|_|\___|\__,_|_|    |_|_| |_| |_|\__,_|\__, |\___| \n"
-printf "                                            |___/       \n"
+echo "clean image"
 
 for imageName in `docker images $1 |sort -u`
 do
