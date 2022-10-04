6529d98c07bb1505a9c2a3ed03dbc60d2168693e
Ming Qiu <mingqiu@xmu.edu.cn>
2022-10-04 13:42:29 +0800

    add docker rmi

--- 3.SpringFrameWork/AdminUI/ClearImage.sh
+++ 3.SpringFrameWork/AdminUI/ClearImage.sh
@@ -3,8 +3,8 @@
 # remove the specific docker image
 
 echo "clean image"
-docker images -q $1
-if [ -n $?];then
+cmd='docker images -q $1'
+if [ -n cmd ];then
   docker rmi $(docker images -q $1)
 fi
 
