ffc1c43ac52733ff70d212a027981ee196e0e12a
Ming Qiu <mingqiu@xmu.edu.cn>
2022-10-04 14:42:48 +0800

    add docker rmi

--- 3.SpringFrameWork/AdminUI/clear_image.sh
+++ 3.SpringFrameWork/AdminUI/clear_image.sh
@@ -2,9 +2,9 @@
 # Auth: Ming Qiu  Time: 2022-10-3-13:03
 # remove the specific docker image
 
-check_results=`docker images -q $1 | awk '{print $1}'`
-echo "cmd result is :$check_results"
-for imageID in $check_results
+docker images -q $1 | awk '{print $1}'
+echo "cmd result is :$?"
+for imageID in $?
 do
   echo "clean image ${imageID}"
   docker rmi imageID
