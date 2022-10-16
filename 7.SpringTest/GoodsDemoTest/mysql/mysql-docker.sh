73f3fb06ef7741780c6470883556212ec491b52e
Ming Qiu <mingqiu@xmu.edu.cn>
2022-10-16 09:49:17 +0800

    mysql docker

--- 7.SpringTest/GoodsDemoTest/mysql/mysql-docker.sh
+++ 7.SpringTest/GoodsDemoTest/mysql/mysql-docker.sh
@@ -1,6 +1,4 @@
 #https://github.com/docker-library/docs/tree/master/mysql
-docker pull mysql
-
-docker service create --name mysql --constraint node.labels.database=mysql --network my-net -e MYSQL_ROOT_PASSWORD=123456  -v  -d mysql:latest
+docker service create --name mysql --constraint node.labels.database=mysql --network my-net -e MYSQL_ROOT_PASSWORD=123456  -d mysql:latest
 docker service ps mysql
-docker run -it --network some-network --rm mysql mysql -hsome-mysql -uexample-user -p
+docker exec -it mysql -uroot -p
