dce24a3edb35d6b5d9c5119a71b8873a54f53b0e
Ming Qiu <mingqiu@xmu.edu.cn>
2022-10-16 08:13:00 +0800

    add mysql docker

--- 5.MyBatis/goodsdemo/mysql/mysql-docker.sh
+++ 5.MyBatis/goodsdemo/mysql/mysql-docker.sh
@@ -1,3 +1,6 @@
+#https://github.com/docker-library/docs/tree/master/mysql
 docker pull mysql
 
-docker service create --name mysql --constraint node.labels.database=mysql --network my-net -e MYSQL_ROOT_PASSWORD=
+docker service create --name mysql --constraint node.labels.database=mysql --network my-net -e MYSQL_ROOT_PASSWORD=123456  -v  -d mysql:latest
+docker service ps mysql
+docker run -it --network some-network --rm mysql mysql -hsome-mysql -uexample-user -p
